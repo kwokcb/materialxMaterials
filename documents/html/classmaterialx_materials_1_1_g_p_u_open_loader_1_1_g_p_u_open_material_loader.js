e37cc9dd471c6c96878df5f92226cff029f1f270
Bernard Kwok <kwokcb@gmail.com>
2025-01-16 09:20:29 -0500

    Update with latest info from PhysicallyBased.

--- documents/html/classmaterialx_materials_1_1_g_p_u_open_loader_1_1_g_p_u_open_material_loader.js
+++ documents/html/classmaterialx_materials_1_1_g_p_u_open_loader_1_1_g_p_u_open_material_loader.js
@@ -1,5 +1,6 @@
 var classmaterialx_materials_1_1_g_p_u_open_loader_1_1_g_p_u_open_material_loader =
 [
+    [ "convertPilImageToBase64", "classmaterialx_materials_1_1_g_p_u_open_loader_1_1_g_p_u_open_material_loader.html#a61d16733c41af4d6fe1330d407f66cc3", null ],
     [ "downloadPackage", "classmaterialx_materials_1_1_g_p_u_open_loader_1_1_g_p_u_open_material_loader.html#a78bd0830e40c7ed0f19a3f6792a79b49", null ],
     [ "downloadPackageByExpression", "classmaterialx_materials_1_1_g_p_u_open_loader_1_1_g_p_u_open_material_loader.html#a74e51310848bbd1047b525e82b434fc2", null ],
     [ "extractPackageData", "classmaterialx_materials_1_1_g_p_u_open_loader_1_1_g_p_u_open_material_loader.html#a9868e4fb69f84012a19957c624e0c1dc", null ],
