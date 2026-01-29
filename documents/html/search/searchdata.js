265ce0907b6f050c3e9c8a59dc68d29625449aa5
kwokcb <kwokcb@gmail.com>
2026-01-29 00:20:49 -0500

    - Add jbook page builder to buildDocs - Rebuild docs.

--- documents/html/search/searchdata.js
+++ documents/html/search/searchdata.js
@@ -5,8 +5,7 @@ var indexSectionsWithContent =
   2: "m",
   3: "p",
   4: "_abcdefgilprsvw",
-  5: "acdhlmprsu",
-  6: "m"
+  5: "acdhlmprsu"
 };
 
 var indexSectionNames =
@@ -16,8 +15,7 @@ var indexSectionNames =
   2: "namespaces",
   3: "files",
   4: "functions",
-  5: "variables",
-  6: "pages"
+  5: "variables"
 };
 
 var indexSectionLabels =
@@ -27,7 +25,6 @@ var indexSectionLabels =
   2: "Namespaces",
   3: "Files",
   4: "Functions",
-  5: "Variables",
-  6: "Pages"
+  5: "Variables"
 };
 
--- utilities/buildDocs.sh
+++ utilities/buildDocs.sh
@@ -3,6 +3,7 @@ pushd .
 cd utilities
 python mdhtml.py ../README.html -t template.html --top "." -o .. -of index.html
 python mdhtml.py ../examples/README.html -t template.html --top "../examples" -o ../examples -of index.html
+python mdhtml.py ../examples/materialxMaterials_tutorial.html -t template.html --top "../examples" -o ../examples --iframe -of materialxMaterials_tutorial_out.html
 popd
 
 pushd .
