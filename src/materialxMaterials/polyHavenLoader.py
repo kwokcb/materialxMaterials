de33e7882d7f2281b964d2ae3aa7f280b41bc4e0
kwokcb <kwokcb@gmail.com>
2026-04-27 00:56:27 -0400

    Generalize polyhaven to support equivalent Blender / glTF asset filtering + download.

--- src/materialxMaterials/polyHavenLoader.py
+++ src/materialxMaterials/polyHavenLoader.py
@@ -37,15 +37,21 @@ class PolyHavenLoader:
             "User-Agent": "MTLX_Polyaven_Loader/1.0",  # Required by PolyHaven API
         }
 
+        # Keys for supported asset types
+        self.MTLX_KEY = 'mtlx'
+        self.GLTF_KEY = 'gltf'
+        self.BLEND_KEY = 'blend'
+
         self.logger = logging.getLogger('PolyH')
         logging.basicConfig(level=self.logger.info)
 
-    def fetch_materialx_assets(self, max_items=1, download_id=None):
+    def fetch_materialx_assets(self, max_items=1, download_id=None, download_type=None):
         '''
         Fetch MaterialX assets from PolyHaven API and filter them by resolution.
 
         @param max_items Maximum number of assets to fetch. If None, fetch all.
         @param download_id If set, only fetch asset with this ID.
+        @param download_type Type of asset to download (e.g. 'mtlx', 'blend', 'gltf'). Default is None which means to fetch MaterialX
         @return Tuple (materialx_assets, all_assets, filtered_polyhaven_assets):
             - materialx_assets: Dictionary of MaterialX assets with URLs and texture files.
             - all_assets: All assets returned by PolyHaven API.
@@ -60,9 +66,23 @@ class PolyHavenLoader:
         all_assets = resp.json()
 
         materialx_assets = {}
+        gltf_assets = {}
+        blender_assets = {}
         filtered_polyhaven_assets = {}
 
-        item_count = 0;
+        # Look for 1K, 2K , 4K, and 8K versions
+        resolutions = {
+            "1k": None,
+            "2k": None,
+            "4k": None,
+            "8k": None
+        }
+
+        # Default to fetching MaterialX assets if no download type specified
+        if not download_type:
+            download_type = self.MTLX_KEY
+
+        item_count = 0
         for id, data in all_assets.items():
 
             if download_id and id != download_id:
@@ -76,36 +96,84 @@ class PolyHavenLoader:
             resp.raise_for_status()
             files_data = resp.json()
             #json_string = json.dumps(files_data, indent=4)
+            #print(f"Files data for asset '{id}': {json_string}")
+
+            blend_files = files_data.get(self.BLEND_KEY, [])
+            if blend_files:
+                self.logger.info(f"Found Blender files for '{id}'")
+
+                for resolution_key in resolutions.keys():
+                    res = blend_files.get(resolution_key, None)
+                    if not res:
+                        continue
+
+                    n_k_mtlx = res.get(self.BLEND_KEY)
+                    texture_struct = {}
+                    if n_k_mtlx:
+                        include_files = n_k_mtlx.get("include", {})
+                        for path, data in include_files.items():
+                            texture_url = data.get("url")
+                            texture_struct[path] = texture_url
+                    url = n_k_mtlx.get("url")
+                    res_id = id + '_' + resolution_key
+                    if url:
+                        blender_assets[res_id] = {
+                            "url": url,
+                            "texture_files": texture_struct,
+                            "thumbnail_url": thumbnail_url
+                        }                
+
+                if download_id == id and download_type == self.BLEND_KEY:
+                    break
+
+            gltf_files = files_data.get(self.GLTF_KEY, [])
+            if gltf_files:
+                self.logger.info(f"Found glTF files for '{id}'")
+
+                for resolution_key in resolutions.keys():
+                    res = gltf_files.get(resolution_key, None)
+                    if not res:
+                        continue
+
+                    n_k_mtlx = res.get(self.GLTF_KEY)
+                    texture_struct = {}
+                    if n_k_mtlx:
+                        include_files = n_k_mtlx.get("include", {})
+                        for path, data in include_files.items():
+                            texture_url = data.get("url")
+                            texture_struct[path] = texture_url
+                    url = n_k_mtlx.get("url")
+                    res_id = id + '_' + resolution_key
+                    if url:
+                        gltf_assets[res_id] = {
+                            "url": url,
+                            "texture_files": texture_struct,
+                            "thumbnail_url": thumbnail_url
+                        }      
+
+                # Halt if download_id is specified and matches the current asset ID and download type
+                if download_id == id and download_type == self.GLTF_KEY:
+                    break
+
 
             # Remove all keys other than "mtlx"
-            files_data = {k: v for k, v in files_data.items() if k == "mtlx"}            
-
-            mtlx_files = files_data.get("mtlx", [])
+            #files_data = {k: v for k, v in files_data.items() if k == "mtlx"}            
+            mtlx_files = files_data.get(self.MTLX_KEY, [])
             if mtlx_files:
 
                 self.logger.info(f"Found MaterialX data for '{id}'") 
 
-                # Look for 1K, 2K , 4K, and 8K versions
-                resolutions = {
-                    "1k": None,
-                    "2k": None,
-                    "4k": None,
-                    "8k": None
-                }
                 for resolution_key in resolutions.keys():
                     res = mtlx_files.get(resolution_key, None)
                     if not res:
                         continue
 
-                    n_k_mtlx = res.get("mtlx")
+                    n_k_mtlx = res.get(self.MTLX_KEY)
                     texture_struct = {}
                     if n_k_mtlx:
-                        #self.logger.info(f"Found MaterialX files for '{one_k}'")
                         include_files = n_k_mtlx.get("include", {})
-                        #self.logger.info(f"Found include files for '{id}': {one_k}")
                         for path, data in include_files.items():
                             texture_url = data.get("url")
-                            #self.logger.info("Texture path:", path, "URL:", texture_url)
                             texture_struct[path] = texture_url
                     mtlx_url = n_k_mtlx.get("url")
                     res_id = id + '_' + resolution_key
@@ -115,20 +183,11 @@ class PolyHavenLoader:
                             "texture_files": texture_struct,
                             "thumbnail_url": thumbnail_url
                         }
-                        json_string = json.dumps(materialx_assets[res_id], indent=4)
-                        #self.logger.info(f"Found MaterialX for '{res_id}': {json_string}") 
-                        #self.logger.info(f"Found MaterialX for '{res_id}'") 
-                    # Create folder poly_have_data
 
-                    # Save asset data to JSON file
-                    #with open(f"polyhaven_data/{id}_data.json", "w") as f:
-                    #    json.dump(asset_data, f, indent=4)
-                    #    self.logger.info(f"Saved asset data for '{id}' to polyhaven_data/{id}_data.json")
+            filtered_polyhaven_assets[id] = files_data
 
-                filtered_polyhaven_assets[id] = files_data
-
-            # Halt if download_id is specified and matches the current asset ID        
-            if download_id == id:
+            # Halt if download_id is specified and matches the current asset ID and download type
+            if download_id == id and download_type == self.MTLX_KEY:
                 break
 
             # Halt if max_items is specified and reached
@@ -140,9 +199,102 @@ class PolyHavenLoader:
             #if "materialx" in formats:
             #    materialx_assets[slug] = formats["materialx"]
 
-        return materialx_assets, all_assets, filtered_polyhaven_assets
+        return materialx_assets, all_assets, filtered_polyhaven_assets, blender_assets, gltf_assets
 
-    def download_asset(self, asset_list, convert_exr_to_png=True):
+    def download_gltf_asset(self, asset_list):
+        '''
+        Download glTF asset from PolyHaven.
+
+        @param asset_list Dictionary of glTF assets with URLs and texture files.
+        @return Tuple (id, gltf_ascii, texture_binaries):
+            - id: ID of the downloaded asset.
+            - gltf_ascii: The glTF file as ASCII string.
+            - texture_binaries: List of tuples (path, binary content) for textures and thumbnails
+        '''
+        for id, asset in asset_list.items():
+            url = asset.get("url")
+            if not url:
+                self.logger.info(f"No glTF URL found for '{id}'")
+                continue
+            if not url.endswith(".gltf") and not url.endswith(".glb"):
+                self.logger.info(f"Invalid glTF URL for '{id}': {url}")
+                continue
+
+            resp = requests.get(url, headers=self.HEADERS)
+            resp.raise_for_status()
+            if url.endswith(".glb"):
+                self.logger.info(f"Downloaded glTF binary file {url}, size: {len(resp.content)} bytes")
+                gltf_content = resp.content
+            elif url.endswith(".gltf"):
+                gltf_content = resp.text
+                self.logger.info(f"Download glTF file {url}, length: {len(gltf_content)} characters")   
+
+            texture_binaries = []
+            for path, texture_url in asset.get("texture_files", {}).items():
+                self.logger.info(f"Download texture from {texture_url} ...")
+                texture_resp = requests.get(texture_url, headers=self.HEADERS)
+                texture_resp.raise_for_status()            
+                texture_content = texture_resp.content
+                texture_binaries.append((path, texture_content))
+
+            thumbnail_url = asset.get("thumbnail_url")
+            if thumbnail_url:
+                self.logger.info(f"Download thumbnail from {thumbnail_url} ...")                
+                thumbnail_resp = requests.get(thumbnail_url, headers=self.HEADERS)
+                thumbnail_resp.raise_for_status()
+                
+                clean_url = thumbnail_url
+                # Strip any ? or # from the URL
+                clean_url = clean_url.split('?')[0].split('#')[0]
+                clean_url = clean_url.split('/')[-1]  # Get the last part of the URL
+                extension = Path(clean_url).suffix.lower()
+                texture_binaries.append((f"{id}_thumbnail{extension}", thumbnail_resp.content))
+            return id, gltf_content, texture_binaries
+
+    def download_blender_asset(self, asset_list):
+        '''
+        Download Blender asset from PolyHaven.
+
+        @param asset_list Dictionary of Blender assets with URLs and texture files.
+        @return Tuple (id, blend_binary, texture_binaries):
+            - id: ID of the downloaded asset.
+            - blend_binary: The Blender file as binary content.
+            - texture_binaries: List of tuples (path, binary content) for textures and thumbnails.
+        '''
+        for id, asset in asset_list.items():
+            url = asset.get("url")
+            if not url:
+                self.logger.info(f"No Blender URL found for '{id}'")
+                continue
+
+            resp = requests.get(url, headers=self.HEADERS)
+            resp.raise_for_status()
+            blend_binary = resp.content
+            self.logger.info(f"Download Blender file {url}, size: {len(blend_binary)} bytes")   
+
+            texture_binaries = []
+            for path, texture_url in asset.get("texture_files", {}).items():
+                self.logger.info(f"Download texture from {texture_url} ...")
+                texture_resp = requests.get(texture_url, headers=self.HEADERS)
+                texture_resp.raise_for_status()            
+                texture_content = texture_resp.content
+                texture_binaries.append((path, texture_content))
+
+            thumbnail_url = asset.get("thumbnail_url")
+            if thumbnail_url:
+                self.logger.info(f"Download thumbnail from {thumbnail_url} ...")                
+                thumbnail_resp = requests.get(thumbnail_url, headers=self.HEADERS)
+                thumbnail_resp.raise_for_status()
+                
+                clean_url = thumbnail_url
+                # Strip any ? or # from the URL
+                clean_url = clean_url.split('?')[0].split('#')[0]
+                clean_url = clean_url.split('/')[-1]  # Get the last part of the URL
+                extension = Path(clean_url).suffix.lower()
+                texture_binaries.append((f"{id}_thumbnail{extension}", thumbnail_resp.content))
+            return id, blend_binary, texture_binaries                
+
+    def download_mtlx_asset(self, asset_list, convert_exr_to_png=True):
         '''
         Download MaterialX asset and its textures from PolyHaven.
 
@@ -265,6 +417,64 @@ class PolyHavenLoader:
                 #self.logger.info(mtlx_string)
 
             return id, mtlx_string, texture_binaries
+
+    def save_blender_with_textures(self, id, blend_binary, texture_binaries, data_folder, extract_zip=False):
+        '''
+        Save Blender file and texture binaries to a zip file.
+
+        @param id The ID of the Blender asset.
+        @param blend_binary The Blender file as binary content.
+        @param texture_binaries List of tuples (path, binary content) for textures and thumbnails.
+        @param data_folder Folder to save the zip file.
+        @param extract_zip If True, extract the zip file after saving zip.
+        @return None
+        '''
+        # Create a zip file with Blender file and textures
+        filename = f"{id}_blender.zip"
+        filename = Path(data_folder) / filename
+        with zipfile.ZipFile(filename, "w") as zipf:
+            # Write Blender file
+            zipf.writestr(f"{id}.blend", blend_binary)
+            # Write texture files
+            for path, content in texture_binaries:
+                zipf.writestr(path, content)
+        self.logger.info(f"Saved zip: {filename}")
+
+        # Save zip contents to folder
+        extract_folder = Path(data_folder) / f"{id}_blender"
+        if extract_zip:
+            with zipfile.ZipFile(filename, "r") as zipf:
+                zipf.extractall(extract_folder)
+            self.logger.info(f"Extracted zip contents to folder: {extract_folder}")
+
+    def save_gltf_with_textures(self, id, gltf_ascii, texture_binaries, data_folder, extract_zip=False):
+        '''
+        Save glTF file and texture binaries to a zip file.
+
+        @param id The ID of the glTF asset.
+        @param gltf_ascii The glTF file as ASCII string.
+        @param texture_binaries List of tuples (path, binary content) for textures and thumbnails.
+        @param data_folder Folder to save the zip file.
+        @param extract_zip If True, extract the zip file after saving zip.
+        @return None
+        '''
+        # Create a zip file with glTF file and textures
+        filename = f"{id}_gltf.zip"
+        filename = Path(data_folder) / filename
+        with zipfile.ZipFile(filename, "w") as zipf:
+            # Write glTF file
+            zipf.writestr(f"{id}.gltf", gltf_ascii)
+            # Write texture files
+            for path, content in texture_binaries:
+                zipf.writestr(path, content)
+        self.logger.info(f"Saved zip: {filename}")
+
+        # Save zip contents to folder
+        extract_folder = Path(data_folder) / f"{id}_gltf"
+        if extract_zip:
+            with zipfile.ZipFile(filename, "r") as zipf:
+                zipf.extractall(extract_folder)
+            self.logger.info(f"Extracted zip contents to folder: {extract_folder}")
 
     def save_materialx_with_textures(self, id, mtlx_string, texture_binaries, data_folder, extract_zip=False):
         '''
--- src/materialxMaterials/polyHavenLoaderCmd.py
+++ src/materialxMaterials/polyHavenLoaderCmd.py
@@ -16,6 +16,7 @@ def PolyHavenLoaderCmd():
     '''
     parser = argparse.ArgumentParser(description="Fetch MaterialX assets from PolyHaven")
     parser.add_argument("-id", "--download_id", type=str, default="", help="Filter ID to fetch MaterialX assets (e.g. 'polystyrene')")
+    parser.add_argument("-dt", "--download_type", type=str, default="mtlx", help="Type of asset to download (e.g. 'mtlx', 'blend', 'gltf')")
     parser.add_argument("-res", "--download_resolution", type=str, default="1k", help="Resolution of the MaterialX assets to download (e.g. '1k', '2k', '4k', '8k') ")
     parser.add_argument("-fe", "--fetch", action='store_true', help="Fetch and save the MaterialX assets to a file")
     parser.add_argument("-l", "--load", action='store_true', help="Load the MaterialX assets")
@@ -25,7 +26,12 @@ def PolyHavenLoaderCmd():
     parser.add_argument('-x', '--extract_zip', action='store_true', help="Extract downloaded ZIP files")
 
     args = parser.parse_args()
-    data_file = "polyhaven_materialx_assets.json"
+    download_type = args.download_type.lower()
+    all_data_file = 'polyhaven_assets.json'
+    filtered_data_file = 'filtered_polyhaven_assets.json'
+    blend_data_file = "polyhaven_blender_assets.json"
+    gltf_data_file = "polyhaven_gltf_assets.json"
+    mtlx_data_file = "polyhaven_materialx_assets.json"
     fetch = args.fetch
     download_id = args.download_id
     resolution = args.download_resolution
@@ -35,60 +41,119 @@ def PolyHavenLoaderCmd():
     loader = polyHavenLoader.PolyHavenLoader()
 
     materialx_assets = None
+    blend_assets = None
+    gltf_assets = None
 
     if fetch:
         fetch_count = args.count
         if fetch_count and fetch_count < 1:
             fetch_count = 1
-        fetch_location = Path(data_folder) / data_file
-        logger.info(f"Fetching MaterialX assets to {fetch_location}...")
-        materialx_assets, all_assets, filtered_polyhaven_assets = loader.fetch_materialx_assets(download_id=download_id, max_items=fetch_count)
-        with open(fetch_location, "w") as f:
-            logger.info(f"- Saving MaterialX assets to {fetch_location}...")
-            json.dump(materialx_assets, f, indent=4)
+        materialx_assets, all_assets, \
+            filtered_polyhaven_assets, blender_assets, gltf_assets = \
+            loader.fetch_materialx_assets(max_items=fetch_count, download_id=download_id, download_type=args.download_type)
 
         # Write all_assets to JSON file:        
-        all_location = Path(data_folder) / "polyhaven_assets.json"
+        all_location = Path(data_folder) / all_data_file
         with open(all_location, "w") as f:
             json.dump(all_assets, f, indent=4)
             logger.info(f"Saved all assets to {all_location}")
-        # Write filtered assets to JSON file:
-        filtered_location = Path(data_folder) / "filtered_polyhaven_assets.json"
+
+        # Write filtered_polyhaven_assets to JSON file:
+        filtered_location = Path(data_folder) / filtered_data_file
         with open(filtered_location, "w") as f:
             json.dump(filtered_polyhaven_assets, f, indent=4)
             logger.info(f"Saved MaterialX assets to {filtered_location}")
 
+        # Write filtered MTLX, BLENDER, and GLTF assets to JSON files:
+        mtlx_fetch_location = Path(data_folder) / mtlx_data_file
+        with open(mtlx_fetch_location, "w") as f:
+            logger.info(f"- Saving MaterialX assets to {mtlx_fetch_location}...")
+            json.dump(materialx_assets, f, indent=4)
+
+        blender_location = Path(data_folder) / blend_data_file
+        with open(blender_location, "w") as f:
+            json.dump(blender_assets, f, indent=4)
+            logger.info(f"Saved Blender assets to {blender_location}")
+
+        gltf_location = Path(data_folder) / gltf_data_file
+        with open(gltf_location, "w") as f:
+            json.dump(gltf_assets, f, indent=4)
+            logger.info(f"Saved glTF assets to {gltf_location}")
+
     elif load:
-        load_location = Path(data_folder) / data_file
-        if not load_location.exists():
-            load_location = Path(__file__).parent / "data" / "PolyHavenMaterialX" / data_file
+        file_list = [mtlx_data_file, blend_data_file, gltf_data_file]
+        for data_file in file_list:
+            load_location = Path(data_folder) / data_file
             if not load_location.exists():
-                logger.info(f"No MaterialX assets found at {load_location}. Please run with --fetch to fetch assets first.")
-                return
-        
-        with open(load_location, "r") as f:
-            logger.info(f"Loaded MaterialX assets from {load_location}")
-            materialx_assets = json.load(f)
-        #json_string = json.dumps(materialx_assets, indent=4)
-        #logger.info(f"MaterialX assets: {json_string}")
+                load_location = Path(__file__).parent / "data" / "PolyHavenMaterialX" / data_file
+                if not load_location.exists():
+                    logger.info(f"Assets not found at {load_location}. Please run with --fetch to fetch assets first.")
+                    return
+            
+            with open(load_location, "r") as f:
+                logger.info(f"Loaded assets from {load_location}")
+                if data_file == mtlx_data_file:
+                    materialx_assets = json.load(f) 
+                elif data_file == blend_data_file:
+                    blend_assets = json.load(f)
+                elif data_file == gltf_data_file:
+                    gltf_assets = json.load(f)
+            #json_string = json.dumps(materialx_assets, indent=4)
+            #logger.info(f"MaterialX assets: {json_string}")
 
     keep_exr = args.keep_exr if args.keep_exr else False
+    if args.download_type.lower() in ['blend']:
+        keep_exr = True
+
     convert_exr_to_png = not keep_exr
     extract_zip = args.extract_zip if args.extract_zip else False
-    if materialx_assets and download_id:
-        # Find download entry by ID
-        entry_id = download_id + '_' + resolution
-        entry = materialx_assets.get(entry_id)
-        if entry:
-            logger.info(f"Downloading asset with ID '{download_id}', resolution '{resolution}'")
-            asset_list = {entry_id: entry, resolution: resolution}
-            id, mtlx_string, texture_binaries = loader.download_asset(asset_list, convert_exr_to_png)    
-            #logger.info(mtlx_string)            
-            loader.save_materialx_with_textures(id, mtlx_string, texture_binaries, data_folder, extract_zip)
-        else:
-            logger.info(f"No asset found with ID '{entry_id}' in the MaterialX assets.")
-    #else:
-    #    logger.info("No operation specified.")
+    
+    if args.download_type.lower() == 'mtlx':
+        # Download asset. For now only supporting MTLX download.
+        if materialx_assets and download_id:
+            # Find download entry by ID
+            entry_id = download_id + '_' + resolution
+            entry = materialx_assets.get(entry_id)
+            if entry:
+                logger.info(f"Downloading asset with ID '{download_id}', resolution '{resolution}'")
+                asset_list = {entry_id: entry, resolution: resolution}
+                id, mtlx_string, texture_binaries = loader.download_mtlx_asset(asset_list, convert_exr_to_png)    
+                #logger.info(mtlx_string)            
+                loader.save_materialx_with_textures(id, mtlx_string, texture_binaries, data_folder, extract_zip)
+            else:
+                logger.info(f"No asset found with ID '{entry_id}' in the MaterialX assets.")
+        #else:
+        #    logger.info("No operation specified.")
+
+    elif args.download_type.lower() == 'blend':
+        if blend_assets and download_id:
+            # Find download entry by ID
+            entry_id = download_id + '_' + resolution
+            entry = blend_assets.get(entry_id)
+            if entry:
+                logger.info(f"Downloading Blender asset with ID '{download_id}', resolution '{resolution}'")
+                asset_list = {entry_id: entry, resolution: resolution}
+                id, blend_binary, texture_binaries = loader.download_blender_asset(asset_list)    
+                loader.save_blender_with_textures(id, blend_binary, texture_binaries, data_folder, extract_zip)
+            else:
+                logger.info(f"No asset found with ID '{entry_id}' in the MaterialX assets.")
+        #else:
+        #    logger.info("No operation specified.")
+
+    elif args.download_type.lower() == 'gltf':
+        if gltf_assets and download_id:
+            # Find download entry by ID
+            entry_id = download_id + '_' + resolution
+            entry = gltf_assets.get(entry_id)
+            if entry:
+                logger.info(f"Downloading glTF asset with ID '{download_id}', resolution '{resolution}'")
+                asset_list = {entry_id: entry, resolution: resolution}
+                id, gltf_ascii, texture_binaries = loader.download_gltf_asset(asset_list)    
+                loader.save_gltf_with_textures(id, gltf_ascii, texture_binaries, data_folder, extract_zip)
+            else:
+                logger.info(f"No asset found with ID '{entry_id}' in the MaterialX assets.")
+        #else:
+        #    logger.info("No operation specified.")
 
 if __name__ == "__main__":
     PolyHavenLoaderCmd()
